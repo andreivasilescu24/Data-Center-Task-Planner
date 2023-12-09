7bda21f9092f1bcf0f953044678dbe547b037be0
Andrei Vasilescu <vasilescuandrei24@gmail.com>
2023-12-09 19:16:44 +0200

    a

--- src/MyDispatcher.java
+++ src/MyDispatcher.java
@@ -8,14 +8,19 @@ public class MyDispatcher extends Dispatcher {
         super(algorithm, hosts);
     }
 
+    int n = hosts.size();
+    int previous_node_index = 0;
     @Override
     public void addTask(Task task) {
-        System.out.println(algorithm);
         if(algorithm == SchedulingAlgorithm.SIZE_INTERVAL_TASK_ASSIGNMENT) {
 
         } else if(algorithm == SchedulingAlgorithm.SHORTEST_QUEUE) {
 
         } else if(algorithm == SchedulingAlgorithm.ROUND_ROBIN) {
+            hosts.get(previous_node_index).addTask(task);
+            previous_node_index = (previous_node_index + 1) % n;
+            System.out.println("Task " + task.getId() + " is assigned to host " + previous_node_index);
+            System.out.println(hosts.get(previous_node_index).getQueueSize());
 
         } else if(algorithm == SchedulingAlgorithm.LEAST_WORK_LEFT) {
 
--- src/MyHost.java
+++ src/MyHost.java
@@ -1,17 +1,37 @@
 /* Implement this class. */
 
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class MyHost extends Host {
+    PriorityBlockingQueue<Task> queue = new PriorityBlockingQueue<>();
+    public int compare(Task t1, Task t2) {
+        if(t1.getPriority() > t2.getPriority()) {
+            return 1;
+        } else if(t1.getPriority() < t2.getPriority()) {
+            return -1;
+        } else {
+            if(t1.getId() > t2.getId()) {
+                return 1;
+            } else if(t1.getId() < t2.getId()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
     @Override
     public void run() {
     }
 
     @Override
     public void addTask(Task task) {
+        queue.add(task);
     }
 
     @Override
     public int getQueueSize() {
-        return 0;
+        return queue.size();
     }
 
     @Override
